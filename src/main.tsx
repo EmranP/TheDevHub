b4f77f56f05286b3bd0450af838c7935221b6fc4
Emran <nightsking12345@gmail.com>
2025-01-11 01:45:31 +0500

    Fixed bug & clear code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import '@styles/global.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from './app/provider/router'
+import './app/styles/global.scss'
 import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
